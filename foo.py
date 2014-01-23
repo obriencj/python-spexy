38ec18621ed657b88b641d1d68061f67ec2d755f
Christopher O'Brien <obriencj@gmail.com>
2014-01-23 12:46:40 -0500

    what were these even for

--- foo.py
+++
@@ -1,7 +0,0 @@
-
-progn = lambda cont, *args: cont(args[-1])
-
-foo = lambda cont: cont(1)
-
-bar = lambda cont: cont(2)
-
--- spexy/foo.py
+++
@@ -1,8 +0,0 @@
-
-def __getattribute__(name):
-	print name
-	return 100
-
-x = 5
-y = 50
-
